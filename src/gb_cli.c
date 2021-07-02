b05256939c65802b9481c832decfbdea27d77548
david <david.day@utexas.edu>
2021-07-02 04:09:30 -0500

    renamed gb and gb2

---
+++ src/gb_cli.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include "../include/cpu.h"
+#include "../include/video.h"
+#include "../include/screen.h"
+#include <gtk-3.0/gtk/gtk.h>
+
+typedef struct GB {
+    CPU cpu;
+    Video video;
+    Screen screen;
+} GB;
+
+static gboolean on_timeout(gpointer user_data) {
+    static int8_t flag = 0;
+    GB *gb = (GB*) user_data;
+
+    //printf("timeout!\n");
+
+    uint64_t prev_cycles = gb->cpu.current_state.CYCLE_COUNT;
+    step_cpu(&(gb->cpu));
+    step_video(&(gb->video), gb->cpu.current_state.CYCLE_COUNT - prev_cycles);
+    if (*(gb->video.ly) == 144) {
+        if (flag == 0) {
+            gtk_widget_queue_draw(GTK_WIDGET(gb->screen.window.wind));
+            //print_buffer(&(gb->video));
+            flag = 1;
+        }
+    } else {
+        flag = 0;
+    }
+
+    return G_SOURCE_CONTINUE;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: ./gb <GB ROM path>\n");
+        exit(-1);
+    }
+    
+    FILE *f = fopen(argv[1], "rb");
+
+    if (f == NULL) {
+        printf("Error: could not open file.\n");
+        exit(-1);
+    }
+
+    GB gb;
+
+    init_cpu(&gb.cpu);
+    init_video(&gb.video, &gb.cpu);
+    init_screen(&gb.screen, gb.video.buffer);
+    load_program(f, &gb.cpu);
+
+    g_timeout_add(1, on_timeout, &gb);
+
+    show_screen(&(gb.screen));
+}
---
+++ src/gb_gui.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "../include/cpu.h"
+#include "../include/video.h"
+#include "../include/screen.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: ./gb <GB ROM path>\n");
+        exit(-1);
+    }
+    
+    FILE *f = fopen(argv[1], "rb");
+
+    if (f == NULL) {
+        printf("Error: could not open file.\n");
+        exit(-1);
+    }
+
+    CPU cpu;
+    Video video;
+    Screen screen;
+
+    init_cpu(&cpu);
+    init_video(&video, &cpu);
+    init_screen(&screen, video.buffer);
+    load_program(f, &cpu);
+
+    uint64_t prev_cycles;
+    uint8_t flag = 0;
+    //show_screen(&screen);
+    while (1) {
+        prev_cycles = cpu.current_state.CYCLE_COUNT;
+        step_cpu(&cpu);
+        step_video(&video, cpu.current_state.CYCLE_COUNT - prev_cycles);
+        if (*video.ly == 144) {
+            if (flag == 0) {
+               //draw_screen(&screen);
+               print_buffer(&video);
+               flag = 1;
+            }
+        } else {
+            flag = 0;
+        }
+        uint16_t cnt = 0;
+        //while (++cnt > 0);
+    }
+}
